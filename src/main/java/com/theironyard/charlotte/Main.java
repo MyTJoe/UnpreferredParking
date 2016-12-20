0390b4993a5cef37079a8f12b0b12bb8cb2ff5dd
Joe Langley <josephelangley@gmail.com>
2016-12-20 14:40:28 -0500

    add maven project files

---
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -0,0 +1,10 @@
+package com.theironyard.charlotte;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        private ArrayList<UpdateLot>
+    }
+}
