784250d5a7e097044488ea746c1a4f8451b2c514
Joe Langley <josephelangley@gmail.com>
2016-12-20 14:50:54 -0500

    adding port

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -9,9 +9,16 @@ import static spark.Spark.before;
 public class Main {
 
     public static void main(String[] args) {
-        Spark.get("/", ((request, response) -> "Hello World!"));
-        before((request, response) -> {
+        String port = System.getenv("PORT");
+
+        if (port != null) {
+            Spark.port(Integer.valueOf(port));
+        }
+
+        Spark.before((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
         });
+
+        Spark.get("/", ((request, response) -> "Hello, World!"));
     }
 }
